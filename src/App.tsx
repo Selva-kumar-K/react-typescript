aef77c2462797fb7a2eadfc61b111473a25ae8a0
Selva-kumar-K <selvanithish2014@gmail.com>
2025-03-19 20:42:33 +0530

    completed exercise-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import EmployeeCard from "./components/exercise-2/Employees";
+import Heading from "./components/exercise-3/Heading";
 
 export default function App() {
+  const name = "Selva";
   const employees = [
     {
       name: "Selva",
@@ -13,10 +15,21 @@ export default function App() {
     },
   ];
   return (
-    <main>
-      {employees.map((employee) => (
-        <EmployeeCard key={employee.name} item={employee} />
-      ))}
-    </main>
+    <>
+      <main>
+        {employees.map((employee) => (
+          <EmployeeCard key={employee.name} item={employee} />
+        ))}
+      </main>
+
+      <Heading>Hello World!</Heading>
+      <Heading>Hello {name}</Heading>
+      <Heading>Hello {name.length}</Heading>
+      <Heading>Hello {name.length && 2}</Heading>
+      <Heading>
+        Hello <strong>{name}</strong>
+      </Heading>
+      <Heading></Heading>
+    </>
   );
 }
